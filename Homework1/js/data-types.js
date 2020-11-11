d8c07cdcf81585658207a6b59028901f9f0e612a
Anna Garnysh <garnysh.a@gmail.com>
2020-11-11 23:24:59 +0300

    js1

---
+++ Homework1/js/data-types.js
@@ -0,0 +1,20 @@
+'use strict';
+
+let height = 5;
+let width = 3;
+let long = 2;
+let S = 2*(height*width+width*long+long*height);
+console.log(S);
+
+let n = 56;
+let a = n%10;
+let b = (n-a)/10;
+let ab = a + b;
+console.log(ab);
+
+let c = 4;
+let e = 7;
+e = c-e+e;
+c = c+c-1 
+console.log(c);
+console.log(e);
